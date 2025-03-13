45d636d2022a05619c499cf9c966a6910a4b572e
Dell <Dell@DESKTOP-KA24F9N>
2025-03-13 22:07:17 +0530

    last commit

--- Employee_Management_Wellfare/src/main/java/Employee_wellfare/controller/EventRegistrationController.java
+++ Employee_Management_Wellfare/src/main/java/Employee_wellfare/controller/EventRegistrationController.java
@@ -3,6 +3,7 @@ package Employee_wellfare.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import Employee_wellfare.service.EventRegistraionService;
 
 @RestController
 @RequestMapping("/event")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EventRegistrationController {
 	
 	@Autowired
